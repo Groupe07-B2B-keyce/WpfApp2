290a449c29a61fc4090fc02b06e89b6bbb3bd5b8
tiako123 <anaellatiako9@gmail.com>
2025-11-24 22:00:24 +0100

    Étape 2 terminée : classes Livre et Electronique avec héritage + override AfficherDetails()

---
+++ WpfApp2/Electronique.cs
@@ -0,0 +1,17 @@
+﻿// Electronique.cs
+using System;
+
+public class Electronique : Article
+{
+	public int GarantieMois { get; set; }
+
+	public Electronique(string nom, decimal prix, int garantie) : base(nom, prix)
+	{
+		GarantieMois = garantie;
+	}
+
+	public override void AfficherDetails()
+	{
+		Console.WriteLine($"Electro: {Nom}, Garantie: {GarantieMois} mois, Prix: {Prix}€");
+	}
+}
